0a113b5b6d80244faa5816bf56aef8532d874a3d
ChangHun Yoon <drskur@amazon.com>
2025-12-29 16:36:22 +0900

    feat: add backend ddb table

--- packages/common/constructs/src/constants/ssm-keys.ts
+++ packages/common/constructs/src/constants/ssm-keys.ts
@@ -2,4 +2,5 @@ export const SSM_KEYS = {
   LANCEDB_STORAGE_BUCKET_NAME: '/idp-v2/lancedb/storage/bucket-name',
   LANCEDB_LOCK_TABLE_NAME: '/idp-v2/lancedb/lock/table-name',
   DOCUMENT_STORAGE_BUCKET_NAME: '/idp-v2/document-storage/bucket-name',
+  BACKEND_TABLE_NAME: '/idp-v2/backend/table-name',
 } as const;
